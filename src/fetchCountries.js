d7843b0f534673390c7b42840191728656d48232
Ratushnyy Andrey <90136453+Oper992@users.noreply.github.com>
2022-03-13 20:10:52 +0200

    5

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,19 +1,10 @@
-// const searchParams = new URLSearchParams({
-//   ["name.official",
-//   "capital",
-//   "population",
-//   "flags.svg",
-//   "languages"]
-// });
-
 export function fetchCountries(name) {
-  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(
-    response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    },
-  );
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`,
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
-// console.log(searchParams.toString());
--- src/index.js
+++ src/index.js
@@ -2,40 +2,49 @@ import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
+const DEBOUNCE_DELAY = 300;
 
 const ref = {
   input: document.querySelector('#search-box'),
   ul: document.querySelector('.country-list'),
+  div: document.querySelector('.country-info'),
 };
 
-const DEBOUNCE_DELAY = 300;
-
 const render = array => {
-  const htmlUl = array
-    .map(({ flags, name }) => {
-      return `<ul><img src="${flags.svg}">${name.official}</ul>`;
-    })
-    .join('');
+  if (array.length === 1) {
+    const arrayLanguages = Object.values(array.languages);
 
+    ref.div.innerHTML = array
+      .map(({ flags, name, capital, population }) => {
+        return `<h1><img src="${flags.svg}" width = "30px" >${name.official}</h1>
+        <p>Capital: ${capital}
+        Population: ${population}
+        Languages: </p>`;
+      })
+      .join('');
+    console.log(...arrayLanguages);
+  }
+  if (array.length >= 2 && array.length <= 10) {
+    ref.ul.innerHTML = array
+      .map(({ flags, name }) => {
+        return `<li class = "country-item"><img src="${flags.svg}" width = "30px" >${name.common}</li>`;
+      })
+      .join('');
+    console.log(array);
+  }
   if (array.length > 10) {
     Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
   }
-  if (array.length >= 2 && array.length <= 10) {
-    ref.ul.insertAdjacentHTML('beforeend', htmlUl);
-  }
-  console.log(htmlUl);
 };
 
 const infoRendering = e => {
   const value = e.target.value.trim();
+  ref.ul.innerHTML = '';
+  ref.div.innerHTML = '';
 
-  if (value === '') {
-    return;
-  } else {
-    fetchCountries(value)
-      .then(array => render(array))
-      .catch(error => console.log(error));
-  }
+  fetchCountries(value)
+    .then(array => render(array))
+    .catch(error => console.log(error));
 };
 
 ref.input.addEventListener('input', debounce(infoRendering, DEBOUNCE_DELAY));
